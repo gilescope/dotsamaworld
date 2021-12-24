b22cb68cf5c3640c63f9ae9a859b90b455ffcf24
Michael Dorst <m@mdorst.net>
2021-12-24 11:35:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Snake using the Bevy Game Engine
+
+## Prerequisites
+
+```
+cargo install cargo-make
+```
+
+## Build and serve WASM version
+
+Set your local ip address in `Makefile.toml` (`localhost` will work if you want to connect to your own device)
+```toml
+[tasks.serve]
+command = "basic-http-server"
+args = ["-x", "-a", "<your-ip>:4000"]
+dependencies = ["build-web", "basic-http-server"]
+```
+
+```
+cargo make serve
+```
+then point your browser to http://<your-ip>:4000/
+
+
+## Build and run native version
+```
+cargo make run
+```
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::build();
+    app.insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins);
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+    app.add_startup_system(setup.system()).run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // add entities to the world
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
+            .looking_at(Vec3::default(), Vec3::Y),
+        ..Default::default()
+    });
+}
